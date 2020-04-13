61c4c9556dba517a912c20200e8a4a38d7a0c41c
yamif <yamif@DESKTOP-BO4MDH5.homenet.telecomitalia.it>
2020-04-13 12:35:51 +0200

    Inserisce getter e setter

--- ProvaGIT/src/it/begear/corso/provaGIT/ProvaGIT.java
+++ ProvaGIT/src/it/begear/corso/provaGIT/ProvaGIT.java
@@ -4,5 +4,17 @@ public class ProvaGIT {
 	
 	int primo;
 	String secondo;
+	public int getPrimo() {
+		return primo;
+	}
+	public void setPrimo(int primo) {
+		this.primo = primo;
+	}
+	public String getSecondo() {
+		return secondo;
+	}
+	public void setSecondo(String secondo) {
+		this.secondo = secondo;
+	}
 
 }
